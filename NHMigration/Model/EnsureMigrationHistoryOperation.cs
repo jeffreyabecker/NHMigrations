62b6d02e17499990876f3b675b4f1d153b0d043c
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2014-07-09 14:48:54 -0400

    cleaned up old artifiacts

---
+++ NHMigration/Model/EnsureMigrationHistoryOperation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NHibernate.Mapping;
+using NHibernate.SqlTypes;
+using NHMigration.Versioning;
+
+namespace NHMigration.Model
+{
+    public class EnsureMigrationHistoryOperation : IOperation
+    {
+        private readonly IVersioningStrategy _versioningStrategy;
+
+        public EnsureMigrationHistoryOperation(IVersioningStrategy versioningStrategy)
+        {
+            _versioningStrategy = versioningStrategy;
+        }
+        public IEnumerable<IMigrationStatement> GetStatements(IMigrationContext context)
+        {
+            return _versioningStrategy.EnsureDbObjectsStatements;
+        }
+    }
+
+    public class UpdateMigrationHistoryToVersion : IOperation
+    {
+        private readonly IVersioningStrategy _versioningStrategy;
+        private readonly string _version;
+
+        public UpdateMigrationHistoryToVersion(IVersioningStrategy versioningStrategy, string version)
+        {
+            _versioningStrategy = versioningStrategy;
+            _version = version;
+        }
+
+        public IEnumerable<IMigrationStatement> GetStatements(IMigrationContext context)
+        {
+            return _versioningStrategy.UpdateVersionToStatements(_version);
+        }
+    }
+}
--- NHMigration/Versioning/IVersioningStrategy.cs
+++ NHMigration/Versioning/IVersioningStrategy.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using NHibernate.Cfg;
 using NHibernate.Mapping.ByCode.Impl;
+using NHMigration.Model;
 
 namespace NHMigration.Versioning
 {
     public interface IVersioningStrategy
     {
-
+        IEnumerable<IMigrationStatement> EnsureDbObjectsStatements { get; }
+        IEnumerable<IMigrationStatement> UpdateVersionToStatements(string version);
     }
 
   
